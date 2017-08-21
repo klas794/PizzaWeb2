117d7443c23e32d48dc8a34ef9e1d5278d424cd3
Klas <klas793@gmail.com>
2017-08-21 12:31:28 +0200

    dish

--- InMemDbPizza/Data/DbInitializer.cs
+++ InMemDbPizza/Data/DbInitializer.cs
@@ -9,12 +9,24 @@ namespace InMemDbPizza.Data
 {
     public class DbInitializer
     {
-        public static void Initialize(UserManager<ApplicationUser> userManager)
+        public static void Initialize(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             var aUser = new ApplicationUser();
             aUser.UserName = "student@test.com";
             aUser.Email = "student@test.com";
             var r = userManager.CreateAsync(aUser, "Pa$$word").Result;
+
+            if (context.Dishes.ToList().Count == 0)
+            {
+                var capricciosa = new Dish { Name = "Capricciosa", Price = 79 };
+
+                var marg = new Dish { Name = "Marguerita", Price = 59 };
+
+                var hawaii = new Dish { Name = "Hawaii", Price = 49 };
+
+                context.AddRange(capricciosa, marg, hawaii);
+                context.SaveChanges();
+            }
         }
     }
 }
